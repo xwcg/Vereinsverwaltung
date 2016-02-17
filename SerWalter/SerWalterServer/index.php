83d3603afd415613ccc2e41761d862652df57a97
Michael Schwarz <das.xwcg@gmail.com>
2016-02-17 23:16:27 +0100

    Wargh merge conflicts

--- SerWalter/SerWalterServer/index.php
+++ SerWalter/SerWalterServer/index.php
@@ -1,31 +1,4 @@
 <?php
-require_once("system/include.php");
-
-$command = $_POST["cmd"];
-$payload = $_POST["payload"];
-$response = [ "success" => false ];
-
-
-if(isset($command) && isset($payload) && isset($payload["user"]) && isset($payload["pass"]))
-{
-    if(Globals::$query->CheckUser($payload["user"], $payload["pass"]))
-    {
-        switch($command)
-        {
-            default:
-                $response["error"] = "INVALID_CMD";
-                break;
-        }
-    }
-    else
-    {
-        $response["error"] = "INVALID_USER";
-    }
-}
-else
-{
-    $response["error"] = "INCOMPLETE_REQUEST";
-}
 
 require_once("system/include.php");
 
